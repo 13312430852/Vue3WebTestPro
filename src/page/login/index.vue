b8502495fe6c12fe7d346018c5f824d969397d5e
yangzq <1569316366@qq.com>
2022-03-14 23:12:02 +0800

    improvement: 登录页

---
+++ src/page/login/index.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="login">
+
+  </div>
+</template>
+
+<style scoped>
+.login {
+  height: 100vh;
+  background: url("../../../public/loginbg.jpg");
+}
+</style>
